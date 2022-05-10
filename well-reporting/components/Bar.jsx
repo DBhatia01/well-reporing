b29fe6ee12bcbfe51a0c66294acc0bdb9d5e3646
Dimple Bhatia <bhatiadimple90@gmail.com>
2022-05-11 01:32:05 +0530

    Specification Placeholders

---
+++ well-reporting/components/Bar.jsx
@@ -0,0 +1,13 @@
+
+function myBar() {
+    return(
+        <div style={{display: "flex", flexDirection: "column",
+        justifyContent: "flex-end", margin: "0 2px"}}>
+<svg width="55" height="80" viewBox="0 0 55 80" fill="none" xmlns="|
+/www.w3.org/2000/svg">
+    <rect width="55" height="80" fill="#2F2B2B" fillOpacity="0.6" />
+</svg>
+</div>
+    )
+}
+export default myBar;
---
+++ well-reporting/components/BarGraph.jsx
@@ -0,0 +1,17 @@
+import styles from "../styles/Home.module.css";
+import Bar from "./Bar"
+import WellActivityData from "../public/WellActivityData.json"
+
+
+function BarGraph() {
+  return (
+     <a className={styles.card}>
+       <h3>BarGraph</h3>
+       <h4>Daily Activity 11th October</h4>
+       {/* <Bar/>   */}
+     </a>
+  ); 
+}
+
+
+export default BarGraph;
